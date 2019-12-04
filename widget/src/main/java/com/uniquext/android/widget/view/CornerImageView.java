71e6a369c3d47cb2fa7b9405f9709058a8cf100d
uniquext <penghaitao918@163.com>
2019-12-04 17:21:01 +0800

    CornerImageView增加描边

--- widget/src/main/java/com/uniquext/android/widget/view/CornerImageView.java
+++ widget/src/main/java/com/uniquext/android/widget/view/CornerImageView.java
@@ -2,18 +2,16 @@ package com.uniquext.android.widget.view;
 
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.graphics.Bitmap;
-import android.graphics.BitmapShader;
 import android.graphics.Canvas;
-import android.graphics.Matrix;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.Path;
-import android.graphics.Shader;
-import android.graphics.drawable.BitmapDrawable;
 import android.util.AttributeSet;
 
-import com.uniquext.android.widget.R;
-
+import androidx.annotation.ColorInt;
 import androidx.appcompat.widget.AppCompatImageView;
+
+import com.uniquext.android.widget.R;
 
 /**
  * 　 　　   へ　　　 　／|
@@ -38,9 +36,14 @@ import androidx.appcompat.widget.AppCompatImageView;
 public class CornerImageView extends AppCompatImageView {
 
     private final Path path = new Path();
-    private final Matrix matrix = new Matrix();
+    private final Paint paint = new Paint();
+
     private boolean circle = false;
     private float[] radii = new float[]{0, 0, 0, 0, 0, 0, 0, 0};
+
+    @ColorInt
+    private int strokeColor = Color.TRANSPARENT;
+    private float strokeWidth = 0;
 
     public CornerImageView(Context context) {
         this(context, null);
@@ -53,6 +56,7 @@ public class CornerImageView extends AppCompatImageView {
     public CornerImageView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         this.initStyleable(attrs);
+        this.paint.setStyle(Paint.Style.STROKE);
     }
 
     @Override
@@ -63,6 +67,7 @@ public class CornerImageView extends AppCompatImageView {
             path.addRoundRect(0, 0, getWidth(), getHeight(), radii, Path.Direction.CW);
         }
         canvas.clipPath(path);
+        addStroke(canvas);
         super.onDraw(canvas);
     }
 
@@ -75,6 +80,9 @@ public class CornerImageView extends AppCompatImageView {
         float topRightRadius = typedArray.getDimension(R.styleable.CornerImageView_topRightRadius, 0);
         float bottomLeftRadius = typedArray.getDimension(R.styleable.CornerImageView_bottomLeftRadius, 0);
         float bottomRightRadius = typedArray.getDimension(R.styleable.CornerImageView_bottomRightRadius, 0);
+
+        strokeColor = typedArray.getColor(R.styleable.CornerImageView_strokeColor, Color.TRANSPARENT);
+        strokeWidth = typedArray.getDimension(R.styleable.CornerImageView_strokeWidth, 0);
 
         if (cornerRadius != 0) {
             for (int i = 0; i < 8; ++i) {
@@ -97,6 +105,14 @@ public class CornerImageView extends AppCompatImageView {
 
         typedArray.recycle();
         postInvalidate();
+    }
+
+    private void addStroke(Canvas canvas) {
+        if (strokeWidth > 0) {
+            paint.setColor(strokeColor);
+            paint.setStrokeWidth(strokeWidth);
+            canvas.drawPath(path, paint);
+        }
     }
 
     public void setCircle(boolean circle) {
@@ -130,11 +146,27 @@ public class CornerImageView extends AppCompatImageView {
         postInvalidate();
     }
 
-    private BitmapShader initBitmapShader(BitmapDrawable drawable) {
-        Bitmap bitmap = drawable.getBitmap();
-        BitmapShader bitmapShader = new BitmapShader(bitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
-        matrix.setScale(getWidth() * 1.0f / bitmap.getWidth(), getHeight() * 1.0f / bitmap.getHeight());
-        bitmapShader.setLocalMatrix(matrix);
-        return bitmapShader;
+    public void setCornerRadius(float topLeftRadius, float topRightRadius, float bottomLeftRadius, float bottomRightRadius) {
+        radii[0] = radii[1] = topLeftRadius;
+        radii[2] = radii[3] = topRightRadius;
+        radii[6] = radii[7] = bottomLeftRadius;
+        radii[4] = radii[5] = bottomRightRadius;
+        postInvalidate();
+    }
+
+    public void setStrokeColor(@ColorInt int strokeColor) {
+        this.strokeColor = strokeColor;
+        postInvalidate();
+    }
+
+    public void setStrokeWidth(float strokeWidth) {
+        this.strokeWidth = strokeWidth;
+        postInvalidate();
+    }
+
+    public void setStroke(float strokeWidth, @ColorInt int strokeColor) {
+        this.strokeColor = strokeColor;
+        this.strokeWidth = strokeWidth;
+        postInvalidate();
     }
 }
