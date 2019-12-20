7b37b17d79b16828638b85a08746c327d7680bf0
uniquext <penghaitao918@163.com>
2019-12-20 16:20:36 +0800

    文字轮播

--- app/src/main/java/com/uniquext/android/sample/widget/MainActivity.java
+++ app/src/main/java/com/uniquext/android/sample/widget/MainActivity.java
@@ -3,11 +3,21 @@ package com.uniquext.android.sample.widget;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.uniquext.widget.loopplayer.TextSwitchView;
+import com.uniquext.widget.loopplayer.TextTest;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        TextSwitchView textSwitchView = findViewById(R.id.switcher);
+//        textSwitchView.setResources(autoRes);
+//        textSwitchView.setTextStillTime(3000);
+
     }
 }
